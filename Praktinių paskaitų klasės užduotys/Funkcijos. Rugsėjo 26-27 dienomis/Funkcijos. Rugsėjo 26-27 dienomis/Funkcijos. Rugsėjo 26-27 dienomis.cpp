139b9f36405a709357b7e785e0c92ae0a59b7286
Edgar Zukovski <edgar.zukovski@agmail.com>
2024-10-02 17:03:52 +0300

    pakeitimas 1

---
+++ Praktinių paskaitų klasės užduotys/Funkcijos. Rugsėjo 26-27 dienomis/Funkcijos. Rugsėjo 26-27 dienomis/Funkcijos. Rugsėjo 26-27 dienomis.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+void compareNumbers(int a, int b) {
+    if (a > b) {
+        std::cout << "Didesnis skaičius: " << a << std::endl;
+    }
+    else if (a < b) {
+        std::cout << "Didesnis skaičius: " << b << std::endl;
+    }
+    else {
+        std::cout << "Skaičiai lygūs." << std::endl;
+    }
+}
+
+int main() {
+    int numb1; int numb2;
+
+    cin >> numb1; cin numb2;
+
+
+}
