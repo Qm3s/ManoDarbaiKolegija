7b3a2102162d67af1480e9ad07bb587b87474b80
Edgar Žukovski <s056165@ad.viko.lt>
2024-10-03 15:22:29 +0300

    privet

---
+++ Praktinių paskaitų klasės užduotys/Spalio 3-4 d. užduotys/Spalio 3-4 d. užduotys/Spalio 3-4 d. užduotys.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int suskaičiuotiPerimetra(int krastine) {
+    return krastine * 4;  
+}
+
+
+void suskaiciuotiPlota(int krastine, int plotas) {
+    plotas = krastine * krastine;  
+}
+
+int main() {
+    int krastine, perimetras, plotas;
+
+   
+    cout << "Iveskite kvadrato krastine: ";
+    cin >> krastine;
+
+    
+    perimetras = suskaičiuotiPerimetra(krastine);
+    cout << "Kvadrato perimetras yra: " << perimetras << endl;
+
+    
+    suskaiciuotiPlota(krastine, plotas);
+    cout << "Kvadrato plotas yra: " << plotas << endl;
+
+    return 0;
+}
