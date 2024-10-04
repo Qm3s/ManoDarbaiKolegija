7564552c604e9855e2926e370f32f187b6b6a65d
Edgar Žukovski <s056165@ad.viko.lt>
2024-10-04 09:53:03 +0300

    spalio 4d

--- Praktinių paskaitų klasės užduotys/Spalio 3-4 d. užduotys/Spalio 3-4 d. užduotys/Spalio 3-4 d. užduotys.cpp
+++ Praktinių paskaitų klasės užduotys/Spalio 3-4 d. užduotys/Spalio 3-4 d. užduotys/Spalio 3-4 d. užduotys.cpp
@@ -3,28 +3,67 @@ using namespace std;
 
 
 int suskaičiuotiPerimetra(int krastine) {
-    return krastine * 4;  
+    return krastine * 4;
 }
-
-
-void suskaiciuotiPlota(int krastine, int plotas) {
-    plotas = krastine * krastine;  
+void suskaiciuotiPlota(int krastine, int &plotas) {
+    plotas = krastine * krastine;
+} 
+//ploto ir pirimetro funkcijos  ////ploto ir pirimetro funkcijos
+double celsiusToFahrenhei(double tempCelcius) {
+    return tempCelcius * 1.8 + 32;
+}
+void fahrenheitToCelsius(double tempFahrenheit, double& tempCelsius) {
+    tempCelsius = (tempFahrenheit - 32) / 1.8;
 }
 
 int main() {
-    int krastine, perimetras, plotas;
+    cout << "Pasirinkite operacija\n1.Kvadrato pirimetras ir plotas\n2.Celsijus į Farenheitas" << endl;
+    int pasirinkimas;
+    cin >> pasirinkimas;
+    switch (pasirinkimas)
+    {
+        
+    case 1:
+        int krastine, perimetras, plotas;
+        
+        cout << "Iveskite kvadrato krastine: ";
+        cin >> krastine;
 
-   
-    cout << "Iveskite kvadrato krastine: ";
-    cin >> krastine;
+        perimetras = suskaičiuotiPerimetra(krastine);
+        cout << "Kvadrato perimetras yra: " << perimetras << endl;
+
+        suskaiciuotiPlota(krastine, plotas);
+        cout << "Kvadrato plotas yra: " << plotas << endl;
+        break;
+    case 2:
+        int pasirinkimas2;
+        double tempFarenheit;
+        double tempCelcius;
+        cout << "1.C to F.\n2.F to C" << endl;
+        cin >> pasirinkimas2;
+        if (pasirinkimas2==1)
+        {
+            cout << "Iveskite C reiksme" << endl;
+            cin >> tempCelcius;
+            tempFarenheit = celsiusToFahrenhei(tempCelcius);
+            cout << tempCelcius << "C = F" << tempFarenheit;
+        }
+        else
+        {
+            cout << "Iveskite F reiksme" << endl;
+            cin >> tempFarenheit;
+            fahrenheitToCelsius(tempFarenheit, tempCelcius);
+            cout << tempFarenheit << "F = C" << tempCelcius;
+
+        }
+        
+        break;
+
+        
+       
+    }
 
     
-    perimetras = suskaičiuotiPerimetra(krastine);
-    cout << "Kvadrato perimetras yra: " << perimetras << endl;
-
-    
-    suskaiciuotiPlota(krastine, plotas);
-    cout << "Kvadrato plotas yra: " << plotas << endl;
 
     return 0;
 }
