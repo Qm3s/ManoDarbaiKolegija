1be907e9262cb496980d5972e0afacff0069c8bf
Edgar Žukovski <s056165@ad.viko.lt>
2024-12-11 13:58:32 +0200

    prak100%FinalVersion

--- Praktiniai Darbai Pazimiui/Prakt4/Prakt4/Prakt4.cpp
+++ Praktiniai Darbai Pazimiui/Prakt4/Prakt4/Prakt4.cpp
@@ -4,7 +4,6 @@
 #include <fstream>
 using namespace std;
 
-
 struct menuItemType {
     string menuItem;
     double menuPrice;
@@ -21,17 +20,12 @@ menuItemType menuList[8] = {
     {"Arbata", 0.75},
 };
 
-
-struct menuItemOrder
-{
+struct menuItemOrder {
     string menuItem;
     double menuPrice;
     double amount;
 };
-menuItemOrder menuOrder[8] = {
-    
-
-};
+menuItemOrder menuOrder[8] = {};
 
 void clearConsole() {
     for (int i = 0; i < 100; i++) {
@@ -39,180 +33,129 @@ void clearConsole() {
     }
 }
 
-
 void mainMenu() {
-    
-    
-    cout << "1.Show menu" << endl;
-    cout << "2.Uzsakuti patiekalus" << endl;
-    cout << "3.Gauti saskaita" << endl;
-    
+    cout << "1. Show menu" << endl;
+    cout << "2. Uzsakuti patiekalus" << endl;
+    cout << "3. Gauti saskaita" << endl;
 }
 
 void showMenu() {
     clearConsole();
     cout << "------------------------------------------------" << endl;
-    for (int i = 0; i < masyvo_dydis; i++)
-    {
-        cout << i + 1 << ". " << menuList[i].menuItem << " - $" << menuList[i].menuPrice << endl;
-    }
-    cout << "------------------------------------------------" << endl;
-    
-}
-void showOrder() {
+    cout << left << setw(3) << "#" << " | "
+        << setw(35) << "Patiekalo pavadinimas" << " | "
+        << "Kaina (EUR)" << endl;
     cout << "------------------------------------------------" << endl;
     for (int i = 0; i < masyvo_dydis; i++) {
-        
+        cout << left << setw(3) << i + 1 << " | "
+            << setw(35) << menuList[i].menuItem << " | "
+            << fixed << setprecision(2) << menuList[i].menuPrice << endl;
+    }
+    cout << "------------------------------------------------" << endl;
+}
+
+void showOrder() {
+    clearConsole();
+    cout << "------------------------------------------------" << endl;
+    cout << left << setw(3) << "#" << " | "
+        << setw(35) << "Patiekalo pavadinimas" << " | "
+        << setw(10) << "Kaina (EUR)" << " | "
+        << "Kiekis" << endl;
+    cout << "------------------------------------------------" << endl;
+    for (int i = 0; i < masyvo_dydis; i++) {
         if (menuOrder[i].amount > 0) {
-            cout << i + 1 << ". " << menuOrder[i].menuItem << " - $"
-                << menuOrder[i].menuPrice << " - |" << menuOrder[i].amount << "|" << endl;
+            cout << left << setw(3) << i + 1 << " | "
+                << setw(35) << menuOrder[i].menuItem << " | "
+                << setw(10) << fixed << setprecision(2) << menuOrder[i].menuPrice << " | "
+                << setw(6) << menuOrder[i].amount << endl;
         }
     }
     cout << "------------------------------------------------" << endl;
 }
 
-
 void getDataToOrderMenu() {
-    char oneMore = '1';  
-    int i = 0; 
     int choice = 1;
-
-    while (choice != 0) {  
-
+    int i = 0;
+    while (choice != 0) {
         showMenu();
-        
-        printf("Iveskite patiekalo pozicija ( '0' kad iseiti ) : ");
+        cout << "Iveskite patiekalo pozicija ( '0' kad iseiti ): ";
         cin >> choice;
-        if (choice == 0)
-        {
-            break;
-        }
-        
-        printf("\nIveskite kiek noresit: ");  
+        if (choice == 0) break;
+
+        cout << "\nIveskite kiek noresit: ";
         int amount;
         cin >> amount;
 
-        
         menuOrder[i].menuItem = menuList[choice - 1].menuItem;
         menuOrder[i].menuPrice = menuList[choice - 1].menuPrice;
         menuOrder[i].amount = amount;
 
         clearConsole();
-        printf("Sekmingai prideta\n"); 
+        cout << "Sekmingai prideta\n";
         showOrder();
 
-        i++;  
-
-       
-        
+        i++;
         clearConsole();
     }
-    
 }
 
 void howMuch() {
-    printf("Jusu uzsakimas:\n");
+    cout << "Jusu uzsakimas:\n";
     showOrder();
     cout << "------------------------------------------------" << endl;
     double mainPrice = 0;
-    for (int i = 0; i < masyvo_dydis; i++)
-    {
-        mainPrice = mainPrice + menuOrder[i].menuPrice * menuOrder[i].amount;
+    for (int i = 0; i < masyvo_dydis; i++) {
+        mainPrice += menuOrder[i].menuPrice * menuOrder[i].amount;
     }
     cout << fixed << setprecision(2);
-    cout << "Kaina be mokesciu - " << mainPrice << "EUR" << endl;
-    cout << "Mokesciai - " << mainPrice * 0.21 << "EUR" << endl;
-    cout << "Galutine kaina - " << mainPrice + mainPrice * 0.21 << "EUR" << endl;
+    cout << "Kaina be mokesciu - " << mainPrice << " EUR" << endl;
+    cout << "Mokesciai - " << mainPrice * 0.21 << " EUR" << endl;
+    cout << "Galutine kaina - " << mainPrice + mainPrice * 0.21 << " EUR" << endl;
     cout << "------------------------------------------------" << endl;
-   
-        ofstream Failas("Ataskaita.txt");
 
-        
-            
-            Failas << "=============== RESTORANAS ===========" << endl;
-            
-
-            for (int i = 0; i < masyvo_dydis; i++) {
-
-                if (menuOrder[i].amount > 0) {
-                    Failas << i + 1 << ". " << menuOrder[i].menuItem << " - $"
-                        << menuOrder[i].menuPrice << " - |" << menuOrder[i].amount << "|" << endl;
-                }
-            }
-            Failas << "------------------------------------------------" << endl;
-
-            Failas << fixed << setprecision(2);
-            Failas << "Kaina be mokesciu - " << mainPrice << "EUR" << endl;
-            Failas << "Mokesciai - " << mainPrice * 0.21 << "EUR" << endl;
-            Failas << "Galutine kaina - " << mainPrice + mainPrice * 0.21 << "EUR" << endl;
-            Failas << "------------------------------------------------" << endl;
-            
-            
-            Failas.close();
-            cout << "Failas 'Ataskaita.txt' paruostas" << endl;
-        
-        
-
-       
+    ofstream Failas("Ataskaita.txt");
+    Failas << "=============== RESTORANAS ===========" << endl;
+    Failas << left << setw(3) << "#" << " | "
+        << setw(35) << "Patiekalo pavadinimas" << " | "
+        << setw(10) << "Kaina (EUR)" << " | "
+        << "Kiekis" << endl;
+    Failas << "------------------------------------------------" << endl;
+    for (int i = 0; i < masyvo_dydis; i++) {
+        if (menuOrder[i].amount > 0) {
+            Failas << left << setw(3) << i + 1 << " | "
+                << setw(35) << menuOrder[i].menuItem << " | "
+                << setw(10) << fixed << setprecision(2) << menuOrder[i].menuPrice << " | "
+                << setw(6) << menuOrder[i].amount << endl;
+        }
     }
+    Failas << "------------------------------------------------" << endl;
+    Failas << "Kaina be mokesciu - " << mainPrice << " EUR" << endl;
+    Failas << "Mokesciai - " << mainPrice * 0.21 << " EUR" << endl;
+    Failas << "Galutine kaina - " << mainPrice + mainPrice * 0.21 << " EUR" << endl;
+    Failas << "------------------------------------------------" << endl;
+    Failas.close();
 
+    cout << "Failas 'Ataskaita.txt' paruostas" << endl;
+}
 
-
-
-
-
-
-
-int main()
-{ 
-    cout << "=======RESTORANAS===" << endl;
-    while (true)
-    {
-
-
+int main() {
+    cout << "======= RESTORANAS ===" << endl;
+    while (true) {
         mainMenu();
         int operacija;
         cin >> operacija;
 
-        if (operacija == 1)
-        {
+        if (operacija == 1) {
             showMenu();
         }
-        if (operacija == 2)
-        {
-            
+        else if (operacija == 2) {
             getDataToOrderMenu();
             showOrder();
         }
-        if (operacija == 3)
-        {
+        else if (operacija == 3) {
             clearConsole();
             howMuch();
             return 0;
         }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     }
 }
-
